a9ab44b86e612f152c57af988a94a044021641ea
Naveen Kumar K <naveenwashere@gmail.com>
2014-11-21 23:15:14 +0530

    reducer.py
    Changed the reducer logic in such a way that the check for mirroring is removed from reducer. Reducer will not be faster compared to previous logic. Also, the "dict" being maintained is removed, hence reducing the memory consumed.

--- session_two/question_four/mapper.py
+++ session_two/question_four/mapper.py
@@ -11,8 +11,16 @@ import sys
 for line in sys.stdin:
     data = line.strip().split(" ")
     if len(data) != 2:
-	continue
+        continue
 
     value, key = data
+
+    #Get the first and last char of the key
+    #Check which one is lower and then print the key such that the lower character comes first (same 
+    #can be done by reversing the logic such that the higher charcter comes first)
+    #For example for two mirroring keys: ACGT and TGCA. The logic can print keys as either ACGT and ACGT.
+    #Or TGCA and TGCA. That is, based on first and last characters you either reverse the key or print it as it is.
+    if key[0] > key[len(key)-1]:
+        key = key[::-1]
 
     print "{0}\t{1}".format(key, value)
